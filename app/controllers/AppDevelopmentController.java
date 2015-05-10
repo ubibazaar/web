5e2ae07835ad7f6b6b4d1c6a210732b0c5a7b505
Simon Stastny <stastny.simon@gmail.com>
2015-05-10 21:59:05 +0200

    adding apps reworked, now step with adding properties added

--- app/controllers/AppDevelopmentController.java
+++ app/controllers/AppDevelopmentController.java
@@ -1,12 +1,23 @@
 package controllers;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.ubicollab.ubibazaar.core.App;
+import org.ubicollab.ubibazaar.core.ManagerType;
 import org.ubicollab.ubibazaar.core.Platform;
 import org.ubicollab.ubibazaar.core.Category;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 import play.data.Form;
 import play.mvc.Result;
@@ -17,29 +28,51 @@ public class AppDevelopmentController extends UbibazaarController {
 
   public static Form form = Form.form();
   
-  public static Result form() {
+  public static Result step1() {
     List<Platform> platforms = UbibazaarService.platformService.getList();
     Category categories = UbibazaarService.categoryService.get("");
-    return ok(app_registration.render(platforms, categories));
+    return ok(app_registration_step1.render(platforms, categories));
   }
-
+  
+  public static Result step2() {
+    Form requestForm = form.bindFromRequest();
+    String name = (String) requestForm.data().get("name");
+    String platform = (String) requestForm.data().get("platform");
+    String description = (String) requestForm.data().get("description");
+    
+    List<ManagerType> managerTypes = UbibazaarService.managerTypeService.query(ImmutableMap.of("platform", platform));
+    
+    Table<ManagerType, String, String> propertyTable = HashBasedTable.create();
+    
+    for (ManagerType managerType : managerTypes) {
+      Map<String, String> props = new Gson().fromJson(managerType.getInstallationMethod().getProperties(), new TypeToken<Map<String, String>>() {}.getType());
+      
+      for (Entry<String, String> property : props.entrySet()) {
+        propertyTable.put(managerType, property.getKey(), property.getValue());
+      }
+    }
+    
+    return ok(app_registration_step2.render(name, platform, description, propertyTable));
+  }
+  
   public static Result add() {
-    // load form user data
     Form requestForm = form.bindFromRequest();
     String name = (String) requestForm.data().get("name");
     String platform = (String) requestForm.data().get("platform");
     String description = (String) requestForm.data().get("description");
 
+    // find properties
+    Map<String, String> receivedProperties = findReceivedProperties(requestForm, platform);
+    
     // instantiate
     App app = new App();
     app.setName(name);
     app.setPlatform(UbibazaarService.platformService.get(platform));
     app.setAuthor(fetchUserFromSession()); // logged user is the owner
     app.setDescription(description);
+    app.setProperties(new Gson().toJson(receivedProperties));
     
-    //FIXME set categories and properties
-    //app.setProperties(null);
-    //app.setCategory(null);
+    //FIXME categories
     
     // register app in api
     String createdDeviceResourceUrl = UbibazaarService.appService.create(app, session());
@@ -55,6 +88,25 @@ public class AppDevelopmentController extends UbibazaarController {
     } else {
       throw new RuntimeException("No app id in response.");
     }
+  }
+
+  private static Map<String, String> findReceivedProperties(Form requestForm, String platform) {
+    List<ManagerType> managerTypes = UbibazaarService.managerTypeService.query(ImmutableMap.of("platform", platform));
+    
+    Map<String, String> sentProperties = Maps.newHashMap();
+    
+    for (ManagerType managerType : managerTypes) {
+      Map<String, String> knownProperties = new Gson().fromJson(managerType.getInstallationMethod().getProperties(), new TypeToken<Map<String, String>>() {}.getType());
+      
+      for (Entry<String, String> knownProperty : knownProperties.entrySet()) {
+        String propertyName = knownProperty.getKey();
+        
+        if(requestForm.data().containsKey(propertyName)) {
+          sentProperties.put(propertyName, (String)requestForm.data().get(propertyName));
+        }
+      }
+    }
+    return sentProperties;
   }
 
 }
